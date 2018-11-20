7a20cd60091f7ac7b270688a89ab433d98603b88
Christian Krause <me@ckrause.org>
2018-11-20 16:23:42 +0100

    new programs

--- programs/templates/T02.asm
+++ programs/templates/T02.asm
@@ -1,7 +1,13 @@
-lpb $0,1
-  lpb $2,1
+mov $2,$0
+lpb $2,1
+  lpb $4,1
+    sub $4,$3
   lpe
-  lpb $3,1
+  lpb $5,1
+    sub $5,$3
   lpe
-  sub $0,1
+  lpb $6,1
+    sub $6,$3
+  lpe
+  sub $2,1
 lpe

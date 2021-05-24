7f5ec45c9349de691923fad8b8b47c80717fc873
Christian Krause <me@ckrause.org>
2021-05-24 16:41:15 +0200

    updated 16/28839 programs

---
+++ programs/oeis/020/A020337.asm
@@ -0,0 +1,39 @@
+; A020337: Numbers whose base-9 representation is the juxtaposition of two identical strings.
+; 10,20,30,40,50,60,70,80,738,820,902,984,1066,1148,1230,1312,1394,1476,1558,1640,1722,1804,1886,1968,2050,2132,2214,2296,2378,2460,2542,2624,2706,2788,2870,2952,3034,3116,3198,3280,3362,3444,3526,3608,3690,3772
+
+mov $10,$0
+mov $12,$0
+add $12,1
+lpb $12
+  clr $0,10
+  mov $0,$10
+  sub $12,1
+  sub $0,$12
+  mov $6,$0
+  mov $8,2
+  lpb $8
+    clr $0,6
+    mov $0,$6
+    sub $8,1
+    add $0,$8
+    sub $0,1
+    lpb $0
+      cal $2,13954 ; a(n) = sigma_6(n), the sum of the 6th powers of the divisors of n.
+      mov $5,$0
+      mod $0,8
+      add $5,$2
+    lpe
+    mov $1,$5
+    mov $9,$8
+    mul $9,$5
+    add $7,$9
+  lpe
+  min $6,1
+  mul $6,$1
+  mov $1,$7
+  sub $1,$6
+  mul $1,72
+  add $1,10
+  add $11,$1
+lpe
+mov $1,$11
--- programs/oeis/120/A120525.asm
+++ programs/oeis/120/A120525.asm
@@ -1,22 +1,20 @@
 ; A120525: First differences of successive generalized meta-Fibonacci numbers A120503.
 ; 1,1,1,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,1,1,0,1,1,1,0,1
 
-mov $2,$0
-mov $4,2
-lpb $4
-  mov $0,$2
-  sub $4,1
-  add $0,$4
+mov $3,2
+mov $5,$0
+lpb $3
+  mov $0,$5
+  sub $3,1
+  add $0,$3
   sub $0,1
   max $0,0
   cal $0,120503 ; Generalized meta-Fibonacci sequence a(n) with parameters s=0 and k=3.
-  mul $0,2
-  mov $3,$0
-  mov $5,$4
-  mul $5,$0
-  add $1,$5
+  mov $2,$3
+  mul $2,$0
+  add $1,$2
+  mov $4,$0
 lpe
-min $2,1
-mul $2,$3
-sub $1,$2
-div $1,2
+min $5,1
+mul $5,$4
+sub $1,$5

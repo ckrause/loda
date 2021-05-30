a89bd2d31865f3c69f5a3efa5fb0e331da692b2b
Christian Krause <me@ckrause.org>
2021-05-30 11:30:48 +0200

    updated 12/29075 programs

--- programs/oeis/294/A294129.asm
+++ programs/oeis/294/A294129.asm
@@ -6,6 +6,4 @@ lpb $0
   cal $2,85 ; Number of self-inverse permutations on n letters, also known as involutions; number of standard Young tableaux with n cells.
   sub $0,1
   add $1,$2
-  add $1,$2
 lpe
-div $1,2

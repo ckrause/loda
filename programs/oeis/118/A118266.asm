cb1111bd079260fb9f68a737b5b1957aab77be71
Christian Krause <me@ckrause.org>
2021-05-31 17:43:29 +0200

    updated 11/29209 programs

--- programs/oeis/118/A118266.asm
+++ programs/oeis/118/A118266.asm
@@ -1,70 +1,69 @@
 ; A118266: Coefficient of q^n in (1-q)^5/(1-5q); dimensions of the enveloping algebra of the derived free Lie algebra on 5 letters.
 ; 1,0,10,40,205,1024,5120,25600,128000,640000,3200000,16000000,80000000,400000000,2000000000,10000000000,50000000000,250000000000,1250000000000,6250000000000,31250000000000,156250000000000
 
-mov $19,$0
-mov $21,2
-lpb $21
-  clr $0,19
-  mov $0,$19
-  sub $21,1
-  add $0,$21
+mov $18,$0
+mov $20,2
+lpb $20
+  clr $0,18
+  mov $0,$18
+  sub $20,1
+  add $0,$20
   sub $0,1
-  mov $15,$0
-  mov $17,2
-  lpb $17
-    clr $0,15
-    mov $0,$15
-    sub $17,1
-    add $0,$17
+  mov $14,$0
+  mov $16,2
+  lpb $16
+    clr $0,14
+    mov $0,$14
+    sub $16,1
+    add $0,$16
     sub $0,1
-    mov $11,$0
-    mov $13,2
-    lpb $13
-      clr $0,11
-      mov $0,$11
-      sub $13,1
-      add $0,$13
+    mov $10,$0
+    mov $12,2
+    lpb $12
+      clr $0,10
+      mov $0,$10
+      sub $12,1
+      add $0,$12
       sub $0,1
-      mov $7,$0
-      mov $9,2
-      lpb $9
-        mov $0,$7
-        sub $9,1
-        add $0,$9
+      mov $6,$0
+      mov $8,2
+      lpb $8
+        mov $0,$6
+        sub $8,1
+        add $0,$8
         sub $0,1
         max $0,0
-        cal $0,110595 ; a(1)=5. For n > 1, a(n) = 4*5^(n-1) = A005054(n).
+        cal $0,5054 ; a(n) = (4*5^n + 0^n) / 5.
         mov $1,$0
-        mov $10,$9
-        mul $10,$0
-        add $8,$10
+        mov $9,$8
+        mul $9,$0
+        add $7,$9
       lpe
-      min $7,1
-      mul $7,$1
-      mov $1,$8
-      sub $1,$7
-      mov $14,$13
-      mul $14,$1
-      add $12,$14
+      min $6,1
+      mul $6,$1
+      mov $1,$7
+      sub $1,$6
+      mov $13,$12
+      mul $13,$1
+      add $11,$13
     lpe
-    min $11,1
-    mul $11,$1
-    mov $1,$12
-    sub $1,$11
-    mov $18,$17
-    mul $18,$1
-    add $16,$18
+    min $10,1
+    mul $10,$1
+    mov $1,$11
+    sub $1,$10
+    mov $17,$16
+    mul $17,$1
+    add $15,$17
   lpe
-  min $15,1
-  mul $15,$1
-  mov $1,$16
-  sub $1,$15
-  mov $22,$21
-  mul $22,$1
-  add $20,$22
+  min $14,1
+  mul $14,$1
+  mov $1,$15
+  sub $1,$14
+  mov $21,$20
+  mul $21,$1
+  add $19,$21
 lpe
-min $19,1
-mul $19,$1
-mov $1,$20
-sub $1,$19
-div $1,5
+min $18,1
+mul $18,$1
+mov $1,$19
+sub $1,$18
--- programs/oeis/171/A171525.asm
+++ programs/oeis/171/A171525.asm
@@ -1,15 +1,11 @@
 ; A171525: Numerator of (n-th noncomposite/n).
 ; 1,1,1,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227
 
-sub $0,1
-lpb $0
-  mov $2,$0
-  mov $0,1
-  max $2,0
-  cal $2,171577 ; 0, 1 and primes > 3.
-  add $1,$2
-  mul $1,2
-lpe
-div $1,4
+cal $0,8578 ; Prime numbers at the beginning of the 20th century (today 1 is no longer regarded as a prime).
+dif $0,3
+mul $0,3
+mov $1,$0
+sub $1,3
+div $1,6
 mul $1,2
 add $1,1

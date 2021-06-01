49f64ff994a64d3cd66963035cbe335f5c76f5b5
Christian Krause <me@ckrause.org>
2021-06-01 17:52:56 +0200

    updated 19/29209 programs

--- programs/oeis/151/A151922.asm
+++ programs/oeis/151/A151922.asm
@@ -1,19 +1,11 @@
 ; A151922: Number of first-quadrant cells (including the two boundaries) that are "ON" after n-th stage of the Holladay-Ulam cellular automaton.
 ; 1,3,5,9,11,15,19,29,31,35,39,49,53,63,73,101,103,107,111,121,125,135,145,173,177,187,197,225,235,263,291,373,375,379,383,393,397,407,417,445,449,459,469,497,507,535,563,645
 
-mov $2,$0
-mov $4,$0
-lpb $4
-  mov $0,$2
-  sub $4,1
-  sub $0,$4
-  cal $0,161411 ; First differences of A160410.
-  mov $3,$0
-  div $3,8
-  mul $3,2
-  add $3,4
-  add $1,$3
+mul $0,2
+lpb $0
+  mov $2,$0
+  cal $2,79314 ; Number of first-quadrant cells (including the two boundaries) born at stage n of the Holladay-Ulam cellular automaton.
+  sub $0,2
+  add $1,$2
 lpe
-div $1,6
-mul $1,2
 add $1,1
--- programs/oeis/339/A339358.asm
+++ programs/oeis/339/A339358.asm
@@ -1,53 +1,36 @@
 ; A339358: Maximum number of copies of a 1234567 permutation pattern in an alternating (or zig-zag) permutation of length n + 11.
 ; 32,64,320,576,1696,2816,6400,9984,19392,28800,50304,71808,116160,160512,244992,329472,480480,631488,887744,1144000,1560416,1976832,2629120,3281408,4271488,5261568,6723840,8186112,10294656,12403200,15379968,18356736,22480800,26604864
 
-mov $14,$0
-mov $16,$0
-add $16,1
-lpb $16
-  clr $0,14
-  mov $0,$14
-  sub $16,1
-  sub $0,$16
-  mov $11,$0
-  mov $13,$0
-  add $13,1
-  lpb $13
-    mov $0,$11
-    sub $13,1
-    sub $0,$13
-    mov $7,$0
-    mov $9,2
-    lpb $9
-      mov $0,$7
-      sub $9,1
-      add $0,$9
+mov $13,$0
+mov $15,1
+lpb $15
+  mov $10,$13
+  mov $12,$13
+  add $12,1
+  sub $15,1
+  lpb $12
+    clr $0,10
+    mov $0,$10
+    sub $12,1
+    sub $0,$12
+    mov $6,$0
+    mov $8,2
+    lpb $8
+      mov $0,$6
+      sub $8,1
+      add $0,$8
       sub $0,1
       div $0,2
-      mov $3,5
-      add $3,$0
-      mov $0,$3
-      sub $0,2
-      mov $2,$0
-      bin $3,5
-      mul $2,$3
-      mul $2,2
-      mov $1,$2
-      mov $10,$9
-      lpb $10
-        mov $8,$1
-        sub $10,1
-      lpe
+      max $0,0
+      cal $0,40977 ; a(n) = binomial(n+5,5)*(n+3)/3.
+      add $4,$0
+      mov $9,$8
+      mul $9,$4
+      add $7,$9
     lpe
-    lpb $7
-      mov $7,0
-      sub $8,$1
-    lpe
-    mov $1,$8
-    div $1,6
+    mov $1,$7
     mul $1,32
-    add $12,$1
+    add $11,$1
   lpe
-  add $15,$12
 lpe
-mov $1,$15
+mov $1,$11

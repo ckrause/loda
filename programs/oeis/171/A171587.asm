1692b610ba17de3b75bfe35b47aba6b7f6f74915
Christian Krause <me@ckrause.org>
2021-06-01 23:56:12 +0200

    updated 16/29209 programs

--- programs/oeis/171/A171587.asm
+++ programs/oeis/171/A171587.asm
@@ -1,8 +1,6 @@
 ; A171587: Sequence of the diagonal variant of the Fibonacci word fractal. Sequence of the Fibonacci tile.
 ; 0,1,1,0,1,1,0,0,1,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,1,1,0,0
 
-mov $1,$0
-cal $0,134861 ; Wythoff BAA numbers.
-add $0,$1
+cal $0,134862 ; Wythoff ABB numbers.
 mov $1,$0
 mod $1,2

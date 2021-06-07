2e314921fc13683145b3d05d36f45ecac4e00ad6
Christian Krause <me@ckrause.org>
2021-06-07 20:48:31 +0200

    updated 8/29527 programs

---
+++ programs/oeis/284/A284788.asm
@@ -0,0 +1,9 @@
+; A284788: Even numbers that cannot be represented in at least two ways as the sum of two odd composites.
+; 2,4,6,8,10,12,14,16,18,20,22,24,26,28,32,34,38,40,44,46,52,56,62,68
+
+mov $1,$0
+cal $0,116470 ; All distinct Fibonacci and Lucas numbers.
+div $0,37
+add $1,$0
+mul $1,2
+add $1,2

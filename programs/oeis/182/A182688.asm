756ca525aeda1cd31cce6517b7e430e88d5b6fdc
Christian Krause <me@ckrause.org>
2021-06-20 07:24:24 +0200

    updated 11 programs

--- programs/oeis/182/A182688.asm
+++ programs/oeis/182/A182688.asm
@@ -1,45 +1,13 @@
 ; A182688: a(n) = the largest 1-digit number with exactly n divisors, a(n) = 0 if no such number exists.
 ; 1,7,9,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-mov $11,$0
-mov $13,2
-lpb $13
-  clr $0,11
-  mov $0,$11
-  sub $13,1
-  add $0,$13
-  sub $0,1
-  mov $6,1
-  mov $7,1
-  mov $10,$0
-  add $10,6
-  mov $1,$10
-  lpb $0
-    mov $0,2
-    mul $1,$3
-    mov $6,4
-    sub $10,1
-    mov $7,$10
-    mov $8,$10
-  lpe
-  add $1,$7
-  mov $7,$6
-  add $7,1
-  add $7,$1
-  mul $0,$7
-  trn $8,1
-  add $8,$0
-  mov $1,$8
-  add $1,4
-  mov $14,$13
-  lpb $14
-    mov $12,$1
-    sub $14,1
-  lpe
+sub $2,$0
+mov $0,58
+lpb $0
+  mod $0,5
+  add $0,$2
+  add $1,$0
+  sub $1,2
 lpe
-lpb $11
-  mov $11,0
-  sub $12,$1
-lpe
-mov $1,$12
-sub $1,3
+add $1,10
+mod $1,10
